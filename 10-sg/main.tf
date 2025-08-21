8842924ee08ad1d6616b08a7a54c4f4eb0f3109d
Dinesh <dinesh@vasa.com>
2025-08-21 20:11:16 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -20,6 +20,17 @@ module "bastion" {
     vpc_id = local.vpc_id
 }
 
+module "backend_alb" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/Dineshkumar876/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = var.backend_alb_sg_name
+    sg_description = var.backend_alb_sg_description
+    vpc_id = local.vpc_id
+}
+
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
   from_port         = 22
@@ -27,4 +38,13 @@ resource "aws_security_group_rule" "bastion_laptop" {
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.bastion.sg_id
+}
+
+resource "aws_security_group_rule" "backend_alb-bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.backend_alb.sg_id
 }
--- 10-sg/variable.tf
+++ 10-sg/variable.tf
@@ -25,3 +25,13 @@ variable "bastion_sg_description" {
     default = "created for sg bastion"
   
 }
+
+variable "backend_alb_sg_name" {
+    default = "backend_alb"
+  
+}
+
+variable "backend_alb_sg_description" {
+    default = "created for sg backend_alb"
+  
+}
